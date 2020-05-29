3393abb5f3260c277dcec7e62d33f6dc7b3442b7
Austin Dewey <deweya964@gmail.com>
2020-05-29 14:43:13 -0500

    init

---
+++ pkg/apis/adewey/group.go
@@ -0,0 +1,6 @@
+// Package adewey contains adewey API versions.
+//
+// This file ensures Go source parsers acknowledge the adewey package
+// and any child packages. It can be removed if any other Go source files are
+// added to this package.
+package adewey
---
+++ pkg/apis/adewey/v1/doc.go
@@ -0,0 +1,4 @@
+// Package v1 contains API Schema definitions for the adewey v1 API group
+// +k8s:deepcopy-gen=package,register
+// +groupName=adewey.test
+package v1
